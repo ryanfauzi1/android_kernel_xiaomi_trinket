b995c45a45685752a8a11ac2e240bbfbbe879c9e
Ryan Fauzi <76712935+ryanfauzi1@users.noreply.github.com>
2024-03-23 05:56:19 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -76,7 +76,6 @@ function build_kernel() {
 
     make -j$(nproc --all) O=out ARCH=arm64 fury-perf_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
-                          CC=clang \
                           CROSS_COMPILE=aarch64-linux-gnu- \
                           CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
                           CROSS_COMPILE_COMPAT=arm-linux-gnueabi Image.gz-dtb dtbo.img
