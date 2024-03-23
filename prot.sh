151202e8dc49fd86a693a1c4878373c29b182502
Ryan Fauzi <76712935+ryanfauzi1@users.noreply.github.com>
2024-03-23 18:59:31 +0700

    Create prot.sh

---
+++ prot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo $HOME
+if ! [ -d "$HOME/proton" ]; then
+echo "proton clang not found! Cloning..."
+if ! git clone -q https://github.com/kdrag0n/proton-clang.git --depth=1 -b master ~/proton; then ## ini Clang nya tools untuk membangun/compile kernel nya (tidak semua kernel mendukung clang)
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+echo "isi sekarang: "
+ls
+echo "isi home: "
+ls $HOME
+echo "isi home/proton: "
+ls $HOME/proton
